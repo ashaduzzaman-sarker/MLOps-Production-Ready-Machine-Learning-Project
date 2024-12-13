228fa05d5acd7f985dc5cba039c718eb9341522a
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2024-12-13 17:18:38 +0000

    utilities

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="piash",
+    author_email="ashaduzzaman2505@gmail.com",
+    packages=find_packages()
+)
